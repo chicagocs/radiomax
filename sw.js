d4dbbc4c7d4935305290416a64240ffdebd9bb9b
chicagocs <chicago_computers@hotmail.com>
2026-01-19 17:49:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// v4.1
+// v4.0
 // Para reflejar cambios en recursos estáticos (CSS, JS, Imágenes) cambiar CACHE_VERSION
-const CACHE_VERSION = 'v4.1';
+const CACHE_VERSION = 'v4.0';
 const STATIC_CACHE = `max-static-${CACHE_VERSION}`;
 const API_CACHE = `max-api-${CACHE_VERSION}`;
 
